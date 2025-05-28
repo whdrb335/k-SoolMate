d4466bd5184fb970fd71191a7a6ef6006e9bbd7c
kim <whdrb3353@naver.com>
2025-05-28 13:23:59 +0900

    K-SoolMate 제작 시작

---
+++ src/main/java/project/k_SoolMate/KSoolMateApplication.java
@@ -0,0 +1,13 @@
+package project.k_SoolMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KSoolMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KSoolMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/k_SoolMate/KSoolMateApplicationTests.java
@@ -0,0 +1,13 @@
+package project.k_SoolMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KSoolMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
