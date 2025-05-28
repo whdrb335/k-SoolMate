bdf8ad6394c3c5b4975c63d26aa293080babb5a9
kim <whdrb3353@naver.com>
2025-05-28 17:46:11 +0900

    feat: 유저 서비스 -> 유저 생성, 비밀번호 확인, 비밀번호 변경, 내 정보조회, 내 정보변경 refactor: 중복으로 걸리는 메서드를 메소드로 만듬

--- src/main/java/project/k_SoolMate/domain/user/repository/UserRepository.java
+++ src/main/java/project/k_SoolMate/domain/user/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.k_SoolMate.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    boolean findByLoginId(String loginId);
 }
--- src/main/java/project/k_SoolMate/domain/user/service/UserService.java
+++ src/main/java/project/k_SoolMate/domain/user/service/UserService.java
@@ -1,11 +1,17 @@
 package project.k_SoolMate.domain.user.service;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.k_SoolMate.domain.user.User;
 import project.k_SoolMate.domain.user.dto.UserDTO;
 import project.k_SoolMate.domain.user.repository.UserRepository;
+import project.k_SoolMate.domain.user.request.CreateUserRequest;
+import project.k_SoolMate.domain.user.request.UpdateUserRequest;
+import project.k_SoolMate.exception.user.DuplicateUserIdException;
+import project.k_SoolMate.exception.user.NotFoundUserException;
 
 @Service
 // 기본적 DB 읽기전용 (최적화)
@@ -14,11 +20,71 @@ import project.k_SoolMate.domain.user.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final BCryptPasswordEncoder bCryptPasswordEncoder; // 생성 빈이 무겁기때문에 service에서 생성해주어서 보내준다.
+    
     /**
      * 멤버 생성
      */
     @Transactional
-    public UserDTO createUser()
+    public UserDTO createUser(CreateUserRequest request) {
+        //회원 LoginId 중복 메서드
+        validateDuplicateUser(request.getLoginId());
+        User user = User.createUser(request.getLoginId(), request.getLoginPw(), request.getMemberName(), request.getPhoneNumber(),
+                request.getEmail(), request.getAddress(), bCryptPasswordEncoder);
+        User saveUser = userRepository.save(user);
+        return new UserDTO(saveUser);
+    }
+
+
+
+    /**
+     * 비밀번호 확인 로직
+     */
+    @Transactional
+    public void isMatchPasswd(Long id,String passwd) {
+        User user = getUserFindById(id);
+        user.isMatchPasswd(passwd, bCryptPasswordEncoder);
+    }
+
+    /**
+     * 비밀번호 변경 로직
+     */
+    @Transactional
+    public void changePasswd(Long id, String oldPasswd, String newPasswd) {
+        User user = getUserFindById(id);
+        user.changePasswd(oldPasswd, newPasswd, bCryptPasswordEncoder);
+    }
+
+    /**
+     * 내 정보 조회
+     */
+    public UserDTO getMyInfo(Long id) {
+        User user = getUserFindById(id);
+        return new UserDTO(user);
+    }
+
+    /**
+     * 내 정보 변경
+     */
+    @Transactional
+    public UserDTO updateMyInfo(Long id, UpdateUserRequest request) {
+        User user = getUserFindById(id);
+        user.updateUser(request.getPhoneNumber(), request.getEmail(), request.getAddress());
+        return new UserDTO(user);
+    }
+
+
+    //==중복 메서드==//
+    private User getUserFindById(Long id) {
+       return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundUserException("해당 사용자가 존재하지 않습니다.", HttpStatus.NOT_FOUND));
+    }
+
+    //==회원 로그인 ID 중복 메서드==//
+    private void validateDuplicateUser(String loginId) {
+        if (userRepository.findByLoginId(loginId)) {
+            throw new DuplicateUserIdException("이미 존재하는 아이디 입니다.", HttpStatus.CONFLICT);
+        }
+    }
 
 }
