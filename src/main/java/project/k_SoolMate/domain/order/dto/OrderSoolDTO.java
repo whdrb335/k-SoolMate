8e55fa23b970cd048fdd3b7702d20c98fff0e04b
kim <whdrb3353@naver.com>
2025-12-03 12:27:06 +0900

    refactor: OrderSoolDTO 에 name 수정 - (Sool)로 강제 변화 -> orderSool.getItem()으로 변화

--- src/main/java/project/k_SoolMate/domain/order/dto/OrderSoolDTO.java
+++ src/main/java/project/k_SoolMate/domain/order/dto/OrderSoolDTO.java
@@ -9,7 +9,7 @@ import project.k_SoolMate.domain.item.sool.entity.Sool;
 @AllArgsConstructor
 public class OrderSoolDTO {
     private Long soolId;
-    private String name;
+    private String itemName;
     private int orderPrice;
     private int count;
     private int totalPrice; // option (보여주기 좋음)
@@ -17,14 +17,11 @@ public class OrderSoolDTO {
     private double alcoholPercent;
 
     public OrderSoolDTO(OrderSool orderSool) {
-        Sool sool = (Sool) orderSool.getItem();
-        this.soolId = sool.getId();
-        this.name = sool.getName();
+        this.soolId = orderSool.getId();
+        this.itemName = orderSool.getItem().getName();
         this.orderPrice = orderSool.getOrderPrice();
         this.count = orderSool.getCount();
         this.totalPrice = orderSool.getOrderPrice() * orderSool.getCount();
         // sool 엔티티에서 필요한 정보 추출
-        this.brand = sool.getBrand();
-        this.alcoholPercent = sool.getAlcoholPercent();
     }
 }
