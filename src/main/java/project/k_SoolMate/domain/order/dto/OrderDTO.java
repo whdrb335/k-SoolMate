7836e1e95c8080a1060503698bdd0a495ddb4794
kim <whdrb3353@naver.com>
2025-12-04 14:44:55 +0900

    feat: OrderDTO 에 userName 추가 - OrderDTO 에 userName이 나오게 추가

--- src/main/java/project/k_SoolMate/domain/order/dto/OrderDTO.java
+++ src/main/java/project/k_SoolMate/domain/order/dto/OrderDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import project.k_SoolMate.domain.delivery.dto.DeliveryDTO;
 import project.k_SoolMate.domain.order.entity.Order;
 import project.k_SoolMate.domain.order.entity.OrderStatus;
+import project.k_SoolMate.domain.user.dto.UserDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,13 +15,14 @@ import java.util.List;
 public class OrderDTO {
     private Long orderId;
     private OrderStatus orderStatus;
-
+    private String userName;
     private DeliveryDTO delivery;
     private List<OrderSoolDTO> orderSools;
 
     public OrderDTO(Order order) {
         this.orderId = order.getId();
         this.orderStatus = order.getOrderStatus();
+        this.userName = order.getUser().getName();
         this.delivery = new DeliveryDTO(order.getDelivery());
         this.orderSools = order.getOrderSools().stream().map(OrderSoolDTO::new).toList();
     }
