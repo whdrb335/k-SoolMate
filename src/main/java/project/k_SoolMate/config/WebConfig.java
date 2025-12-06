35ecf9fe2c1390e37ce1fc5d40d066333fba77e0
kim <whdrb3353@naver.com>
2025-12-06 11:44:00 +0900

    feat: WebConfig 에 AdminCheck인터셉터 추가

--- src/main/java/project/k_SoolMate/config/WebConfig.java
+++ src/main/java/project/k_SoolMate/config/WebConfig.java
@@ -4,13 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import project.k_SoolMate.common.interceptor.AdminCheckInterceptor;
 import project.k_SoolMate.common.interceptor.LoginCheckInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final LoginCheckInterceptor loginCheckInterceptor;
-
+    private final AdminCheckInterceptor adminCheckInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
@@ -19,5 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/user/login",
                         "/api/user/save"
                 );
+        // 관리자 체크
+        registry.addInterceptor(adminCheckInterceptor)
+                .addPathPatterns("/api/admin/**");
     }
+
+
 }
