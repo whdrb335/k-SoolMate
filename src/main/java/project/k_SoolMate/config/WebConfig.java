c30a9a28d5749a5ee9f5a24f8923b4bf8ac38656
kim <whdrb3353@naver.com>
2025-12-11 14:54:09 +0900

    feat: 필터 등록

--- src/main/java/project/k_SoolMate/config/WebConfig.java
+++ src/main/java/project/k_SoolMate/config/WebConfig.java
@@ -1,29 +1,36 @@
 package project.k_SoolMate.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import project.k_SoolMate.common.interceptor.AdminCheckInterceptor;
-import project.k_SoolMate.common.interceptor.LoginCheckInterceptor;
+import project.k_SoolMate.common.jwt.AdminAuthorizationFilter;
+import project.k_SoolMate.common.jwt.JwtAuthFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private final LoginCheckInterceptor loginCheckInterceptor;
-    private final AdminCheckInterceptor adminCheckInterceptor;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginCheckInterceptor)
-                .addPathPatterns("/api/user/**", "/api/order/**")
-                .excludePathPatterns(
-                        "/api/user/login",
-                        "/api/user/save"
-                );
-        // 관리자 체크
-        registry.addInterceptor(adminCheckInterceptor)
-                .addPathPatterns("/api/admin/**");
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AdminAuthorizationFilter adminAuthFilter;
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthFilter> jwtFilter() {
+        FilterRegistrationBean<JwtAuthFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(jwtAuthFilter);
+        bean.addUrlPatterns("/*");
+        bean.setOrder(1);
+        return bean;
     }
 
-
+    @Bean
+    public FilterRegistrationBean<AdminAuthorizationFilter> adminFilter() {
+        FilterRegistrationBean<AdminAuthorizationFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(adminAuthFilter);
+        bean.addUrlPatterns("/api/admin/*"); // 관리자 API만 필터 적용
+        bean.setOrder(2);
+        return bean;
+    }
 }
+
