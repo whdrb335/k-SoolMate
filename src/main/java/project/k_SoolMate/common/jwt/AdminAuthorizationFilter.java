240cb178d16503f10e804690380f8fe69031fa59
kim <whdrb3353@naver.com>
2025-12-16 11:09:21 +0900

    refactor: 커스텀 필터로 쓰던 로직

--- src/main/java/project/k_SoolMate/common/jwt/AdminAuthorizationFilter.java
+++ src/main/java/project/k_SoolMate/common/jwt/AdminAuthorizationFilter.java
@@ -1,38 +1,38 @@
-package project.k_SoolMate.common.jwt;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-public class AdminAuthorizationFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain)
-                         throws IOException, ServletException {
-
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-
-        String uri = req.getRequestURI();
-
-        // 여기 반드시 /api/admin
-        if (uri.startsWith("/api/admin")) {
-
-            String role = (String) req.getAttribute("role");
-
-            if (role == null || !role.equals("ADMIN")) {
-                res.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                return;
-            }
-        }
-
-        chain.doFilter(request, response);
-    }
-}
-
+//package project.k_SoolMate.common.jwt;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class AdminAuthorizationFilter implements Filter {
+//
+//    @Override
+//    public void doFilter(ServletRequest request,
+//                         ServletResponse response,
+//                         FilterChain chain)
+//                         throws IOException, ServletException {
+//
+//        HttpServletRequest req = (HttpServletRequest) request;
+//        HttpServletResponse res = (HttpServletResponse) response;
+//
+//        String uri = req.getRequestURI();
+//
+//        // 여기 반드시 /api/admin
+//        if (uri.startsWith("/api/admin")) {
+//
+//            String role = (String) req.getAttribute("role");
+//
+//            if (role == null || !role.equals("ADMIN")) {
+//                res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//                return;
+//            }
+//        }
+//
+//        chain.doFilter(request, response);
+//    }
+//}
+//
